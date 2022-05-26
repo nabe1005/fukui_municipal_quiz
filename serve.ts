7d85b50bd050155b1b65f8109f2fac40289d12ad
nabe1005 <taiki.w.1113@gmail.com>
2022-05-26 22:24:01 +0900

    /images return municipal images

--- serve.ts
+++ serve.ts
@@ -1,11 +1,10 @@
 #!/usr/bin/env -S deno run --allow-net --allow-read --watch
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
-import { serveFile } from "https://deno.land/std@0.140.0/http/file_server.ts";
+import { serveFile, serveDir } from "https://deno.land/std@0.140.0/http/file_server.ts";
 
 serve(async (req) => {
   const pathname = new URL(req.url).pathname;
   console.log(pathname);
-
 
   if (pathname === "/") {
     return await serveFile(req, getFilePath("src/index.html"));
@@ -13,6 +12,15 @@ serve(async (req) => {
 
   if (pathname === "/styles.css") {
     return new Response(await Deno.readTextFile("./src/css/styles.css"), { headers: { "Content-Type": "text/css" } });
+  }
+
+  if (pathname.startsWith("/images")) {
+    return serveDir(req, {
+      fsRoot: "assets/municipal_images",
+      urlRoot: "images",
+      showDirListing: true,
+      enableCors: true,
+    });
   }
 
   return await serveFile(req, getFilePath("src/not-found.html"));
