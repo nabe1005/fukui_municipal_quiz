9a459e0bbf6ea2e6b0dea71a61e1d7b8a8a66811
nabe1005 <taiki.w.1113@gmail.com>
2022-05-27 01:01:51 +0900

    質問リストをシャッフルしてから返すようにした

--- quiz.ts
+++ quiz.ts
@@ -9,6 +9,12 @@ export function createQuestions(): MunicipalQuestion[] {
         questions.push(question);
     });
 
+    // 質問リストをシャッフルする(Fisher-Yates shuffle)
+    for (let i = questions.length - 1; i >= 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [questions[i], questions[j]] = [questions[j], questions[i]];
+    }
+
     return questions;
 }
 
