7f214026fbc6db07bfac84f8890bc0acf141e8a0
nabe1005 <taiki.w.1113@gmail.com>
2022-05-27 00:34:01 +0900

    GET: /api/questions で問題一覧を取得できるようにした

--- serve.ts
+++ serve.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env -S deno run --allow-net --allow-read --watch
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 import { serveFile, serveDir } from "https://deno.land/std@0.140.0/http/file_server.ts";
+import { createQuestions } from "./quiz.ts";
 
 serve(async (req) => {
   const pathname = new URL(req.url).pathname;
@@ -8,6 +9,13 @@ serve(async (req) => {
 
   if (req.method === "GET" && pathname === "/api/mock") {
     return new Response(await Deno.readTextFile("mock.json"), {
+      headers: { "Content-Type": "application/json; charset=utf-8" }
+    });
+  }
+
+  if (req.method === "GET" && pathname === "/api/questions") {
+    const questions = createQuestions();
+    return new Response(JSON.stringify(questions), {
       headers: { "Content-Type": "application/json; charset=utf-8" }
     });
   }
